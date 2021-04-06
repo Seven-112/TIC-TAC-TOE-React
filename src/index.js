fdea1f99e8440a04553139f999052c08473905f0
myshine112 <bluesatellite777@gmail.com>
2021-04-06 23:11:21 +0200

    Taking Turns

--- src/index.js
+++ src/index.js
@@ -14,13 +14,17 @@ class Board extends React.Component {
         super(props);
         this.state = {
           squares: Array(9).fill(null),
+          xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -31,7 +35,7 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
